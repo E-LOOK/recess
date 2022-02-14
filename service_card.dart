3ac8df2d19b4fa663001a4bb28ecfd2038abe269
Ochieng Paul <97807374+OchiengPaul442@users.noreply.github.com>
2022-02-14 23:46:07 +0300

    Add files via upload

--- service_card.dart
+++ service_card.dart
@@ -1,103 +1,52 @@
-import 'package:TheLook/Modules/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'booklogic.dart';
-import 'service.dart';
-
-class ServiceTile extends StatefulWidget {
-  const ServiceTile({
+class ServiceCard extends StatelessWidget {
+  const ServiceCard({
     Key? key,
-    required this.service,
+    required this.image,
+    required this.name,
+    required this.pressed,
+    required this.bgColor,
   }) : super(key: key);
-  final Service service;
+  final String image, name;
+  final VoidCallback pressed;
 
-  @override
-  State<ServiceTile> createState() => _ServiceTileState();
-}
+  final Color bgColor;
 
-class _ServiceTileState extends State<ServiceTile> {
-  bool _isFavorite = true;
   @override
   Widget build(BuildContext context) {
-    final controller = Provider.of<Controller2>(context, listen: false);
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Card(
-          color: cardColor1,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          elevation: 3,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 2 - 40,
-                      child: Text(
-                        widget.service.title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      '${widget.service.duration} min',
-                      style: const TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
+    return GestureDetector(
+      onTap: pressed,
+      child: Container(
+        width: 154,
+        decoration: const BoxDecoration(
+          color: Colors.transparent,
+        ),
+        child: Column(
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+                  backgroundImage: Image.network(image).image,
+                  radius: 40,
                 ),
-                Text(
-                  "\$${widget.service.price} ",
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                MaterialButton(
-                    onPressed: () {},
-                    color: const Color(0xffFF8573),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: TextButton(
-                        child: (_isFavorite
-                            ? const Text('Book')
-                            : const Icon(
-                                Icons.check,
-                                color: Colors.green,
-                              )),
-                        onLongPress: () {},
-                        onPressed: () {
-                          toggleTick();
-                          controller.addProductToCart(widget.service);
-                        })),
               ],
             ),
-          ),
+            Row(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Text(name,
+                        style: const TextStyle(
+                          color: Color.fromRGBO(11, 2, 28, 2),
+                        )),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
-      ],
+      ),
     );
-  }
-
-  void toggleTick() {
-    setState(() {
-      if (_isFavorite) {
-        _isFavorite = false;
-      } else {
-        _isFavorite = true;
-      }
-    });
   }
 }
